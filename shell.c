926f2a6aff7d26e5c240a10e8b9e713eb73f3d31
alexoph <39359838+alexoph@users.noreply.github.com>
2018-05-16 23:08:10 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,3 +1,13 @@
+/*
+* Autores:
+*	Santiago Hernandez Arias Codigo 1631281
+*   Alejandro Palacios Hurtado Codigo 1630531
+* Nombre: shell.c
+* Descripción: Archivo con funcion main para el shell basico
+* Fecha de creación: 4 / 05 / 2018
+* Fecha de modificación: 16 / 05 / 2018
+*/
+
 #include "commandUtil.h"
 #include <fcntl.h>
 #include <stdio.h>
@@ -11,8 +21,10 @@
 int main(int argc, char *argv[]) {
 	while (1) {
 		char comando[80];
+		// Leer comando
 		printf("$ ");
 		fgets(comando, 1024, stdin);
+		// Remover espacios y guardar en modifiedCommand
 		char *modifiedCommand = removeExtraSpaces(comando);
 		int pfd_1[2];
 
@@ -24,6 +36,7 @@ int main(int argc, char *argv[]) {
 		char **args2 = malloc(64 * sizeof(char *));
 
 		separarArgumentos(args1, modifiedCommand);
+		// Chequea si hay pipe en el comando, separa izquierda y derecha si lo hay
 
 		int piper = checkpip(args1, args2);
 
@@ -39,6 +52,7 @@ int main(int argc, char *argv[]) {
 		pid_t pid = fork();
 
 		if (!pid) {
+			// Ejecucion cuando es un comando simple (sin pipe)
 			if (!piper) {
 				ejecutr(args1);
 			} else {
